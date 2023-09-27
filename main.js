136c3484565f712574001e3d540f3fe2533d1a72
YKyungmin <xcv556865@gmail.com>
2023-09-27 15:22:16 +0900

    닫기 버튼 모션 추가 및 docodelab 글자 분리

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('main');
 const [top, right, bottom, left, open, content] = main.children;
+const close = content.querySelector('.close');
+const h2 = content.querySelector('h2');
 const speed = 500;
+
+splitText(h2, 0.1);
 
 open.addEventListener('click', () => {
 	new Anime(
@@ -42,3 +46,28 @@ open.addEventListener('click', () => {
 		}
 	);
 });
+
+close.addEventListener('click', () => {
+	content.classList.remove('on');
+
+	setTimeout(() => {
+		new Anime(top, { width: '0%' });
+		new Anime(bottom, { width: '0%' });
+		new Anime(left, { height: '0%' });
+		new Anime(right, { height: '0%' });
+		open.classList.remove('off');
+	}, 1000);
+});
+
+function splitText(selector, interval = 0) {
+	let count = 0;
+	const txt = selector.innerText;
+	selector.innerHTML = '';
+	for (const el of txt) {
+		const span = document.createElement('span');
+		span.innerText = el;
+		span.style.transitionDelay = `${interval * count}s`;
+		selector.append(span);
+		count++;
+	}
+}
